41134f4c935932e179eaef312f40f4a3a767ac08
thesnake94 <dromdhane@guardiaschool.fr>
2023-02-14 10:59:31 +0100

    km + energie

--- scrapCsv.py
+++ scrapCsv.py
@@ -24,7 +24,6 @@ soup = BeautifulSoup(response.text, 'html.parser')
 
 # Trouver tous les éléments searchCard
 searchCard_elements = soup.find_all(class_='searchCard')   
-brand = soup.find_all("body")
 
 # Liste pour stocker les données
 data_scrap = []
@@ -39,8 +38,8 @@ for result in searchCard_elements:
    price = result.find(class_="Text_Text_text Vehiculecard_Vehiculecard_price Text_Text_subtitle2")
    location = result.find(class_="Vehiculecard_Vehiculecard_location")
    year = result.find(class_="Text_Text_text Vehiculecard_Vehiculecard_characteristicsItems Text_Text_body2")
-   # fuel = result.find(class_="Vehiculecard_Vehiculecard_characteristics")
-   # mileage = result.find(class_="Vehiculecard_Vehiculecard_characteristics")
+   fuel = soup.find_all(class_="Text_Text_text Vehiculecard_Vehiculecard_characteristicsItems Text_Text_body2")[3]
+   mileage = soup.find_all(class_="Text_Text_text Vehiculecard_Vehiculecard_characteristicsItems Text_Text_body2")[1]
    a_element = result.find("a")
    link = a_element.get("href")
 
@@ -48,23 +47,24 @@ for result in searchCard_elements:
       brand_text = brand.text.split()[0]
       model_text = " ".join(model.text.split()[1:])
 
-      print(model.text, motor.text,'\n', "Année :", year.text,'\n', "Prix :", price.text, '\n',"localisé dans le", location.text, '\n', "www.lacentrale.fr" + link, end='\n''\n''\n')
+      print(model.text, motor.text,'\n', "Année :", year.text,'\n', fuel.text,'\n' , "Prix :", price.text, '\n',"localisé dans le", location.text, '\n', "www.lacentrale.fr" + link, mileage.text, end='\n''\n''\n')
       
 
       # Convertir les chaînes de caractères en entiers
       price_int = int(price.text.replace(" ", "").replace("€", ""))
       year_int = int(year.text)
+      mileage_int = int(mileage.text.replace(" ", "").replace("km", ""))
       location_int = int(location.text)
 
       # Ajouter les données converties à la liste data_scrap
-      data_scrap.append([brand_text, model_text, motor.text, year_int, price_int, fuel.text, location_int])
+      data_scrap.append([brand_text, model_text, motor.text, year_int, price_int, fuel.text, mileage_int, location_int])
 
       
 
 # Écrire les données dans un fichier CSV
 with open("bmw.csv", "w", newline="") as fd:
    writer = csv.writer(fd)
-   writer.writerow(["Marque" ,"Modèle", "Motorisation", "Année", "Prix", "Energie", "Département"]) # Écrire les en-têtes
+   writer.writerow(["Marque" ,"Modèle", "Motorisation", "Année", "Prix", "Energie", "Kilométrage", "Département"]) # Écrire les en-têtes
    for row in data_scrap:
       writer.writerow(row)
 
