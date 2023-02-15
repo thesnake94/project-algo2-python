c331132e80d6daeddade64d803f4110c29c0e0bd
thesnake94 <dromdhane@guardiaschool.fr>
2023-02-15 10:18:55 +0100

    commentaires

--- fonction.py
+++ fonction.py
@@ -3,52 +3,58 @@ import requests
 import csv
 
 def main():
-    for page in range(1, 5):
-    # choisir les filtres
+    # pour toutes les pages de 1 à 5
+    for page in range(1, 6):
+        # choisir les filtres en déclarant les variables pour l'url
         brand = "BMW"
         year_min = 2010
         year_max = 2019
         power_min = 350
         fuel = ""
+
+        # l'url avec les variables dedans
         url = """https://www.lacentrale.fr/listing?energies={fuel}&makesModelsCommercialNames={brand}&options=&page={page}&powerDINMin={power_min}&yearMax={year_max}&yearMin={year_min}""".format(
         brand=brand,fuel=fuel, power_min=power_min,page=page, year_min=year_min, year_max=year_max,)
 
         print("Voici l'url de la page scraper :",'\n', url, end='\n''\n')
 
 
-        # Faire une demande GET à l'URL
+        # faire une demande GET à l'URL
         response = requests.get(url)
 
-        # Créer un objet BeautifulSoup à partir de la réponse HTML
+        # créer un objet BeautifulSoup à partir de la réponse HTML
         soup = BeautifulSoup(response.text, 'html.parser')
 
-        # Trouver tous les éléments searchCard
+        # trouver tous les éléments searchCard
         searchCard_elements = soup.find_all(class_='searchCard')   
 
-   # Liste pour stocker les données
+        # liste pour stocker les données
         data_scrap = []
 
-
+        # definir les variables tant que les éléments sont dans les card
         for scrap in searchCard_elements:
-            brand = scrap.find("h3")
-            model = scrap.find("h3")
-            motor = scrap.find(class_="Text_Text_text Vehiculecard_Vehiculecard_subTitle Text_Text_body2")
-            price = scrap.find(class_="Text_Text_text Vehiculecard_Vehiculecard_price Text_Text_subtitle2")
-            location = scrap.find(class_="Vehiculecard_Vehiculecard_location")
-            year = scrap.find(class_="Text_Text_text Vehiculecard_Vehiculecard_characteristicsItems Text_Text_body2")
-            fuel = scrap.find_all(class_="Text_Text_text Vehiculecard_Vehiculecard_characteristicsItems Text_Text_body2")[3]
-            mileage = scrap.find_all(class_="Text_Text_text Vehiculecard_Vehiculecard_characteristicsItems Text_Text_body2")[1]
+            brand = scrap.find("h3") # marque
+            model = scrap.find("h3") #modele
+            motor = scrap.find(class_="Text_Text_text Vehiculecard_Vehiculecard_subTitle Text_Text_body2") # moteur
+            price = scrap.find(class_="Text_Text_text Vehiculecard_Vehiculecard_price Text_Text_subtitle2") # prix
+            location = scrap.find(class_="Vehiculecard_Vehiculecard_location") # localisation
+            year = scrap.find(class_="Text_Text_text Vehiculecard_Vehiculecard_characteristicsItems Text_Text_body2") # année
+            fuel = scrap.find_all(class_="Text_Text_text Vehiculecard_Vehiculecard_characteristicsItems Text_Text_body2")[3] # energie
+            mileage = scrap.find_all(class_="Text_Text_text Vehiculecard_Vehiculecard_characteristicsItems Text_Text_body2")[1] # kilométrage
             a_element = scrap.find("a")
-            link = a_element.get("href")
+            link = a_element.get("href") # lien de l'annonce
 
             if scrap:
-                brand_text = brand.text.split()[0]
-                model_text = " ".join(model.text.split()[1:])
 
+                # on créer les variables qui récupèrent seulement les mots que l'on veut dans les variables créées avant
+                brand_text = brand.text.split()[0] # pour la variable brand on récupère seulement le premier mot pour la marque
+                model_text = " ".join(model.text.split()[1:]) # pour la variable model on récupère le reste des mots apres le premier du h3 pour le modele
+
+                # on affiche toutes les données récupérées
                 print(model.text, motor.text,'\n', "Année :", year.text,'\n', fuel.text,'\n' , mileage.text,'\n', "Prix :", price.text, '\n',"localisé dans le", location.text, '\n', "www.lacentrale.fr" + link, end='\n''\n''\n')
                 
 
-                # Convertir les chaînes de caractères en entiers
+                # on converti les chaînes de caractères en entiers (string en int)
                 price_int = int(price.text.replace(" ", "").replace("€", ""))
                 year_int = int(year.text)
                 mileage_str = mileage.text.strip().replace("\xa0", "").replace("km", "")
